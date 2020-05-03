52ec39493381438094a5087b90f9c0ebcfca5fad
danielbcook <36240146+danielbcook@users.noreply.github.com>
2020-05-02 18:56:06 -0700

    Update Worst day.sql
    added code blocks for total # tested in US, and total testing positive

--- Worst day.sql
+++ Worst day.sql
@@ -1,11 +1,33 @@
-WITH daily_percentages as
+/*
+-- GRANT SELECT ON ALL TABLES IN SCHEMA "PUBLIC" TO ROLE PUBLIC;
+
+-- total of US tests turning out positive
+WITH latest_data as 
+    (
+        SELECT DATE, SUM(IFNULL(TOTAL,0)) as total_tests, sum(IFNULL(POSITIVE,0)) as cases, sum(IFNULL(DEATH,0)) as deaths
+        FROM COVID.PUBLIC.CT_US_COVID_TESTS
+        WHERE DATE = (SELECT TOP 1 date FROM COVID.PUBLIC.CT_US_COVID_TESTS ORDER BY 1 desc)
+        GROUP BY DATE
+    )
+SELECT
+    latest_data.date
+  , latest_data.total_tests as total_tested
+  , latest_data.cases / total_tested
+FROM latest_data
+WHERE 1=1
+ORDER BY 1 desc
+;
+*/
+
+DROP TABLE IF EXISTS GEO_DATA.PUBLIC.daily_percentages;
+CREATE TABLE GEO_DATA.PUBLIC.daily_percentages as
 (
   WITH state_date_matrix as
   (
         WITH all_dates as
         (
             SELECT DISTINCT date
-            FROM COVID.PUBLIC.CT_US_COVID_TESTS
+            FROM COVID.PUBLIC.CT_US_COVID_TESTS ORDER BY 1 desc
         ),
         states as
         (
@@ -48,8 +70,9 @@ WITH daily_percentages as
             INNER JOIN COVID.PUBLIC.CT_US_COVID_TESTS sd1 ON (sd1.date = sdm1.date AND sd1.ISO3166_2 = sdm1.state_code)
             INNER JOIN state_population ON state_population.state_code = sdm1.state_code
         ORDER BY relative_case_growth desc
-),
-latest_data as 
+);
+
+WITH latest_data as 
     (
         SELECT DATE, ISO3166_2 as state, sum(IFNULL(POSITIVE,0)) as cases, sum(IFNULL(DEATH,0)) as deaths, SUM(IFNULL(TOTAL,0)) as total_tests
         FROM COVID.PUBLIC.CT_US_COVID_TESTS
@@ -69,16 +92,19 @@ SELECT daily_percentages.*
   , latest_data.cases / state_testing.tested as percent_positive_per_test
   , daily_percentages.state as state_code
   , total_population / state_testing.tested as residents_per_test
-  ,(select max(arithmetic_daily_new_events) FROM daily_percentages dp2 WHERE dp2.state = daily_percentages.state) as arithmetic_daily_new_events_join
-FROM daily_percentages
+  , (select max(arithmetic_daily_new_events) FROM daily_percentages dp2 WHERE dp2.state = daily_percentages.state) as arithmetic_daily_new_events_join
+FROM GEO_DATA.PUBLIC.DAILY_PERCENTAGES daily_percentages
     INNER JOIN state_testing ON state_testing.state_code = daily_percentages.state
     INNER JOIN latest_data ON latest_data.state = daily_percentages.state
 WHERE 1=1
     AND daily_percentages.arithmetic_daily_new_events = arithmetic_daily_new_events_join
---    AND daily_percentages.date = (SELECT TOP 1 date FROM COVID.PUBLIC.CT_US_COVID_TESTS ORDER BY 1 desc) ORDER BY arithmetic_daily_new_events desc -- to see only the states which are "moving worst day"
 --    AND daily_percentages.arithmetic_relative_change_percent = arithmetic_relative_change_percent_join
+--    AND daily_percentages.date = (SELECT TOP 1 date FROM COVID.PUBLIC.CT_US_COVID_TESTS ORDER BY 1 desc) ORDER BY arithmetic_daily_new_events desc -- to see only the states which are "moving worst day"
+--    AND daily_percentages.state = 'GA'
 ORDER BY
     daily_percentages.state
-  , arithmetic_daily_new_events desc
   , arithmetic_relative_change_percent desc -- 1st tiebreaker
+  , arithmetic_daily_new_events desc
   , geometric_relative_change_percent desc -- 2nd tiebreaker
+;
+
